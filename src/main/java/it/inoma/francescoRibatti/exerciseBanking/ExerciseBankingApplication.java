1a6ccca675ee96b532c36863ec630909f0a40acb
CiccioFr <cicciofr.net@tiscali.it>
2023-11-16 22:03:41 +0100

    Creazione progetto

---
+++ src/main/java/it/inoma/francescoRibatti/exerciseBanking/ExerciseBankingApplication.java
@@ -0,0 +1,13 @@
+package it.inoma.francescoRibatti.exerciseBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseBankingApplication.class, args);
+	}
+
+}
